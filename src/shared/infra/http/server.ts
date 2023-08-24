add4f856b5f8f12ed4431ccdd13205b587011538
augustoroocha <augustinho_darocha1311@hotmail.com>
2023-08-24 00:31:43 -0300

    use express

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import http from 'http';
 import path from 'path';
 
@@ -11,15 +11,24 @@ const pathPublic = express.static(path.join(__dirname, '..', "public"));
 
 
 app.use(pathPublic);
+app.use(express.json());
 //app.use(createConnectionRouter)
+app.get('/h', (req: Request, res: Response) => {
+    res.json('serverrrrr expresshttp')
+})
 const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
     cors: {
         origin: "*",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-        
+
     }
 });
+
+app.get('/home', (req: Request, res: Response) => {
+    res.json('serverrrrr expresshttp')
+})
+
 
 export { serverHttp, io, PORT }
